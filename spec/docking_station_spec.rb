b1a940cd8eaf1e1c109e6872f55d96db47ba954a
Lewis Jones <lewis.jones1997@protonmail.com>
2021-09-20 16:06:09 +0100

    test(docking_station): test for releasing a working bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,20 +3,11 @@ require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike)}
 
-  # describe 'the release_bike method' do
-
-  #   context 'there is a working bike' do
-  #     it 'releases a bike to the user' do
-        
-  #     end
-  #   end
-
-  #   context 'there is not a working bike' do
-  #     it 'tells the user there are no bikes available' do
-      
-  #     end
-  #   end
-    
-    
-  # end
+  describe "release_bike method" do
+    it "releases a working bike" do
+      result = subject.release_bike
+      expect(result.class).to eq Bike
+      expect(result.working?).to eq true
+    end
+  end
 end
