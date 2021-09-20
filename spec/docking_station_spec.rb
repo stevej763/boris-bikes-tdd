fc78959cb833c7ea7567ff647665dbbb661ec719
Lewis Jones <lewis.jones1997@protonmail.com>
2021-09-20 16:36:31 +0100

    test(docking_station): add failing test for dock_bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,13 +1,17 @@
 require 'docking_station'
 
 describe DockingStation do
-  it { is_expected.to respond_to(:release_bike)}
-
   describe "release_bike method" do
+    it { is_expected.to respond_to(:release_bike)}
+
     it "releases a working bike" do
       result = subject.release_bike
       expect(result.class).to eq Bike
       expect(result.working?).to eq true
     end
+  end
+
+  describe "dock_bike method" do
+    it { is_expected.to respond_to(:dock_bike)}
   end
 end
