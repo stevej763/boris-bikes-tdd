863f2d68c882382d7c943c50683d33201205c6e5
Steve Jones <steve.jones763@gmail.com>
2021-09-20 15:47:36 +0100

    first passing test for BikeStation, responds to release_bike

---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,22 @@
+require 'docking_station'
+
+describe DockingStation do
+  it {respond_to(:release_bike)}
+
+  # describe 'the release_bike method' do
+
+  #   context 'there is a working bike' do
+  #     it 'releases a bike to the user' do
+        
+  #     end
+  #   end
+
+  #   context 'there is not a working bike' do
+  #     it 'tells the user there are no bikes available' do
+      
+  #     end
+  #   end
+    
+    
+  # end
+end
