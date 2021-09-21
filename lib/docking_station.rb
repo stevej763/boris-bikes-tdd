2f2a6395121726c12d0dbf7f1b7e74152814e91c
Steve Jones <steve.jones763@gmail.com>
2021-09-21 14:35:40 +0100

    Refactored DockingStation to follow SRP

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,12 +8,24 @@ class DockingStation
   attr_reader :bikes
 
   def release_bike
-    raise "No available bikes" if @bikes.empty?
+    raise "No available bikes" if empty?
     return @bikes.pop
   end
 
   def dock_bike(bike)
-    raise "Docking station full" if @bikes.count == 20
+    raise "Docking station full" if full?
     @bikes.push(bike)
   end
+
+  private
+  
+  def full?
+    @bikes.count == 20
+  end
+
+  def empty?
+    @bikes.empty?
+  end
+
+  
 end
