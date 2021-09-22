98a70bfe6592fa18460b9b03a22dc87b0999d1d4
Lewis Jones <lewis.jones1997@protonmail.com>
2021-09-22 15:12:59 +0100

    wip: write test for delivering broken bikes to the garage

--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -3,6 +3,7 @@ require 'van'
 describe Van do
   let(:broken_bike) {double(:broken_bike)}
   let(:docking_station) { double :docking_station, release_broken_bikes: [broken_bike] }
+  let(:garage) { double :garage, receive_bikes: [broken_bike] }
 
   describe "#collect_broken_bikes" do
     it { is_expected.to respond_to(:collect_broken_bikes) }
@@ -10,6 +11,15 @@ describe Van do
     it "collects broken bikes from a docking station" do
       subject.collect_broken_bikes(docking_station)
       expect(subject.bikes).to include(broken_bike)
+    end
+  end
+
+  describe "#deliver_broken_bikes" do
+    it "delivers broken bikes to a garage" do
+      expect(garage).to receive(:receive_bikes)
+
+      subject.deliver_broken_bikes(garage)
+      expect(subject.bikes).to eq []
     end
   end
 end
