0ce1bba8a2ea1484ec78857113742685b30574f9
Lewis Jones <lewis.jones1997@protonmail.com>
2021-09-22 14:31:23 +0100

    wip

--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -1,10 +1,12 @@
 require 'van'
 
 describe Van do
+  let(:docking_station) { double(:docking_station) get_broken_bikes: }
   describe "#collect_broken_bikes" do
     it { is_expected.to respond_to(:collect_broken_bikes) }
 
     it "collects broken bikes from a docking station" do
+
       subject.collect_broken_bikes(docking_station)
       expect(subject.bikes).to eq 
     end
