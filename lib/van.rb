b5a93540c6c5735b30f7388e7a13235e4a03b8b0
Steve Jones <steve.jones763@gmail.com>
2021-09-22 15:40:26 +0100

    Failing test on delivering multiple bikes to garage.

--- lib/van.rb
+++ lib/van.rb
@@ -9,5 +9,19 @@ class Van
   def collect_broken_bikes(docking_station)
     @bikes += docking_station.release_broken_bikes
   end
+  
+  def deliver_broken_bikes(garage)
+    bikes_to_fix = []
+    puts "van: #{@bikes}"
+
+    @bikes.each do |bike|
+      puts "bike to delete: #{bike}"
+      bikes_to_fix << bike
+      @bikes.delete(bike)
+    end
+    puts "van: #{@bikes}"
+    puts bikes_to_fix
+    garage.receive_bikes(bikes_to_fix)
+  end
 
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -1,23 +1,24 @@
 require 'van'
 
 describe Van do
-  let(:broken_bike) {double(:broken_bike)}
-  let(:docking_station) { double :docking_station, release_broken_bikes: [broken_bike] }
-  let(:garage) { double :garage, receive_bikes: [broken_bike] }
+  let(:broken_bike) {instance_double(Bike)}
+  let(:broken_bike_2) {instance_double(Bike)}
+  let(:docking_station) { double :docking_station, release_broken_bikes: [broken_bike, broken_bike_2] }
+  let(:garage) { double :garage, receive_bikes: [broken_bike, broken_bike_2] }
 
   describe "#collect_broken_bikes" do
     it { is_expected.to respond_to(:collect_broken_bikes) }
 
     it "collects broken bikes from a docking station" do
       subject.collect_broken_bikes(docking_station)
-      expect(subject.bikes).to include(broken_bike)
+      expect(subject.bikes).to include(broken_bike, broken_bike_2)
     end
   end
 
   describe "#deliver_broken_bikes" do
     it "delivers broken bikes to a garage" do
-      expect(garage).to receive(:receive_bikes)
-
+      subject.collect_broken_bikes(docking_station)
+      expect(garage).to receive(:receive_bikes).with([broken_bike, broken_bike_2])
       subject.deliver_broken_bikes(garage)
       expect(subject.bikes).to eq []
     end
